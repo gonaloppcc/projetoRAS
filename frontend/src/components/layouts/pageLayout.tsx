d000c35dac6aee016d578b7ea51bfd518acde51c
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-10-29 01:16:25 +0100

    Setup project structure.

---
+++ frontend/src/components/layouts/pageLayout.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const PageLayout = () => {
+    // Code a navbar here and return it
+    return <div></div>;
+};
---
+++ frontend/src/constants/Palette.ts
@@ -0,0 +1,13 @@
+export const PALETTE = {
+    WHITE: '#ffffff',
+    CULTURED: '#F5F3F4',
+    BLACK_SHADOW: '#B1A7A6',
+    LIGHT_GRAY: '#D3D3D3',
+    IMPERIAL_RED: '#E5383B',
+    CARNELIAN: '#BA181B',
+    RUBY_RED: '#A4161A',
+    BLOOD_RED: '#660708',
+    EERIE_BLACK: '#161A1D',
+    RICH_BLACK: '#0B090A',
+    SPECIAL: '#E6B800',
+};
