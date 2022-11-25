13411a1d16108cc4d9b91ddbc75b2bb41e4b5b15
Mapc1 <marcocosta579@gmail.com>
2022-11-25 22:16:10 +0000

    Change User GET link

--- backend/RasbetServer/RasbetServer/Controllers/UserController.cs
+++ backend/RasbetServer/RasbetServer/Controllers/UserController.cs
@@ -1,4 +1,6 @@
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RasbetServer.Models;
 
 namespace RasbetServer.Controllers;
@@ -9,9 +11,14 @@ public class UserController : ControllerBase
 {
     public UserController() {}
 
-    [HttpGet("{email}/{password}", Name = "Login")]
-    public ActionResult<User> Login(string email, string password)
+    /// 
+    [HttpGet("[action]", Name = "Login")]
+    public ActionResult<User> Login([FromBody] JsonObject json)
     {
+        var dyn = JsonConvert.DeserializeObject<dynamic>(json.ToString());
+        string email = dyn.email;
+        string password = dyn.password;
+        
         return new User(email, "Marco Costa", password, "12345678", "87654321", "919191919");
     }
 }
