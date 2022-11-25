c21f0a4cd62f2ffcb5662e58e942ca0d4a95afd1
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-11-25 17:19:49 +0000

    Add Page Layout.

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
+import {Navlink} from '@components/Navlink';
+
+const navlinks = [
+    {
+        name: 'Desporto', // FIXME: Name is in portuguese, needs to be generic
+        href: '/', // FIXME: Change this href to the sports page
+        isActive: false, // FIXME Change this to true if the current page is the sports page
+    },
+    {
+        name: 'Promoções', // FIXME: Same as above
+        href: '/',
+        isActive: true,
+    },
+];
 
 export const Navbar = () => {
     return (
         <div className="hidden md:flex flex-row justify-between items-center px-8 h-12 gap-3 bg-IMPERIAL_RED ">
-            <div className="flex flex-row items-center">
+            <div className="flex flex-row items-center gap-8">
                 <Image src={'/logo.png'} width={50} height={50} />
+                <div className="h-full flex flex-row ">
+                    {navlinks.map((navlink) => (
+                        <Navlink key={navlink.name} {...navlink} />
+                    ))}
+                </div>
             </div>
-            <div></div>
         </div>
     );
 };
