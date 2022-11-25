a26ed6227f1eecc66c04e86fc1d1799b5ed38dca
Mapc1 <marcocosta579@gmail.com>
2022-11-25 22:28:59 +0000

    Add documentation and request correctness checking

--- backend/RasbetServer/RasbetServer/Controllers/UserController.cs
+++ backend/RasbetServer/RasbetServer/Controllers/UserController.cs
@@ -11,14 +11,24 @@ public class UserController : ControllerBase
 {
     public UserController() {}
 
+    /// <summary>
+    /// Logs in a user
+    /// </summary>
     /// 
+    /// <param name="json">Json object in the GET body. Must have an email and password</param>
+    /// <returns>The logged in user or an error when the user either has invalid credentials or does not exist</returns>
     [HttpGet("[action]", Name = "Login")]
     public ActionResult<User> Login([FromBody] JsonObject json)
     {
         var dyn = JsonConvert.DeserializeObject<dynamic>(json.ToString());
         string email = dyn.email;
+        if (email is null)
+            return BadRequest("No email was provided");
+
         string password = dyn.password;
-        
+        if (password is null)
+            return BadRequest("No password was provided");
+
         return new User(email, "Marco Costa", password, "12345678", "87654321", "919191919");
     }
 }
