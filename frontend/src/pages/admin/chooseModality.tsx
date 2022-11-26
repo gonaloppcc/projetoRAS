97b796f2b85b1ea2ab7f83d844beee0cf79fb42b
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2022-11-26 11:43:44 +0000

    choose modality correct, i think

--- frontend/src/pages/admin/chooseModality.tsx
+++ frontend/src/pages/admin/chooseModality.tsx
@@ -3,15 +3,39 @@ import Head from 'next/head';
 import Image from 'next/image';
 import {ScrollModalities} from '../../components/RegisterSpecialist/scrollModalities';
 
+// FIXME
 const MOCK_Modalities = ['Futebol', 'Basket', 'Marathon'];
+
 const Home: NextPage = () => {
+    const sportSelected = (sport) => {
+        console.log('Selected sport');
+        console.log(sport);
+    };
+
     return (
         <>
-            <div className="gap-8 h-screen w-screen bg-CULTURED">
-                <div className="relative pl-1/2 h-64 w-64">
-                    <div className="bg-white absolute inset-x-0 top-0 h-16">
-                        Selecione uma modalidade Nao consigo
+            <div className="gap-8 h-screen w-screen justify-center flex items-center bg-CULTURED">
+                <div className=" flex flex-col gap-5 items-top pt-10  min-h-screen w-max  ">
+                    <div className="bg-white h-fit pl-5 pr-96 text-xl inline-block py-2 font-semibold">
+                        {/* TODO: Alterar*/}
+                        Selecione uma modalidade
                     </div>
+                    {MOCK_Modalities.map((mod) => (
+                        <>
+                            <div
+                                className="flex flex-row gap-1 bg-white text-gl py-2 rounded cursor-pointer"
+                                onClick={() => sportSelected(mod)}
+                            >
+                                <div key={`${mod}-1`} className="pr-0">
+                                    {/* TODO: Alterar*/}
+                                    ☺︎
+                                </div>
+                                <div key={`${mod}-2`} className="pl-0">
+                                    {mod}
+                                </div>
+                            </div>
+                        </>
+                    ))}
                 </div>
             </div>
         </>
