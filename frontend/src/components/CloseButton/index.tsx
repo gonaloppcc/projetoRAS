98dbf53d7d42b5a3b9e22a4b30f238922e31ac76
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-11-30 22:09:19 +0000

    Refactor Close Button.

--- frontend/src/components/CloseButton/index.tsx
+++ frontend/src/components/CloseButton/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
-import {IconButton} from '@mui/material';
 
 interface CloseButtonProps {
     onClick?: () => void;
@@ -8,10 +7,8 @@ interface CloseButtonProps {
 
 export const CloseButton = ({onClick}: CloseButtonProps) => {
     return (
-        <div style={{height: '100%'}}>
-            <IconButton onClick={onClick} disableRipple>
-                <CloseIcon fontSize="small" sx={{cursor: 'pointer'}} />
-            </IconButton>
+        <div onClick={onClick} className="text-base cursor-pointer">
+            <CloseIcon />
         </div>
     );
 };
