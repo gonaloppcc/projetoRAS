4d8eb514021c2efb275c77fae161c54b8b8ebec8
Mapc1 <marcocosta579@gmail.com>
2022-12-01 22:03:26 +0000

    Make Team's Player list have Player instead of string

--- backend/RasbetServer/RasbetServer/Models/Events/Participants/Participant/Team.cs
+++ backend/RasbetServer/RasbetServer/Models/Events/Participants/Participant/Team.cs
@@ -2,10 +2,10 @@ namespace RasbetServer.Models.Events.Participants.Participant;
 
 public class Team : Participant
 {
-    public Team(string name, IEnumerable<string> players) : base(name)
+    public Team(string name, IEnumerable<Player> players) : base(name)
     {
         Players = players.ToList();
     }
 
-    public List<string> Players { get; }
+    public List<Player> Players { get; }
 }
