659673b2ca01b0983ee7ec77713d50e7826ee66e
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-12-02 17:15:53 +0000

    Install authentication packages

--- backend/RasbetServer/RasbetServer/app/Server.cs
+++ backend/RasbetServer/RasbetServer/app/Server.cs
@@ -1,9 +1,24 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            //ValidIssuer = Configuration["Jwt:Issuer"],
+            //ValidAudience = Configuration["Jwt:Issuer"],
+            //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+        };
+    });
 
 var app = builder.Build();
 app.UseHttpsRedirection();
