c2cc5ea7ba98b890423a6cb0bd5751f10f291bfc
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2022-12-03 00:20:02 +0000

    está giro...

--- frontend/src/components/RegisterEvent/sucessPage.tsx
+++ frontend/src/components/RegisterEvent/sucessPage.tsx
@@ -9,8 +9,8 @@ export const SucessPage = ({changePage}: SucessPageProps) => {
 
     return (
         <div className="h-screen w-screen justify-center flex items-center bg-CULTURED">
-            <div className="bg-white  flex flex-col items-center px-10 py-10 h-auto  relative gap-2">
-                <div className="w-fit h-10  text-4xl ">
+            <div className="bg-white flex flex-col items-center px-10 py-10 h-auto relative gap-5 ">
+                <div className="w-fit text-4xl">
                     Evento submetido com sucesso
                 </div>
                 <PrimaryButton
