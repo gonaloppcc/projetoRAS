ad8d3c3ad0d53899034164a2a6ebd7858364f9ec
Mapc1 <marcocosta579@gmail.com>
2022-12-03 23:58:23 +0000

    Make Transaction's id a string

--- backend/RasbetServer/RasbetServer/Models/Users/Transaction.cs
+++ backend/RasbetServer/RasbetServer/Models/Users/Transaction.cs
@@ -1,10 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json.Linq;
 
 namespace RasbetServer.Models.Users;
 
-public class Transaction {
-    [Key] public ulong? _id { get; set; }
+public class Transaction
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string? Id { get; set; } = null;
     [Required] public float Value { get; set; }
 
     public Transaction(float value) {
