e4af1f5d0d5773e6871fff8eae51e8492162eb43
Mapc1 <marcocosta579@gmail.com>
2022-12-04 16:29:39 +0000

    Create branch cache-api2

---
+++ backend/RasbetServer/APICache/Program.cs
@@ -0,0 +1,24 @@
+using System.Net.Http.Headers;
+using APICache.lib;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Add("User-Agent", "RASBet Application");
+
+const int refreshRate = 5000; //10 * 60;
+
+var api = new Api();
+
+Console.WriteLine("Please make sure the vpn is connected!");
+
+while (true) {
+        var json = await api.FetchGames(client);
+
+        bool dbChanged = api.WriteToDatabase(json);
+        var time = DateTime.Now;
+        
+        Console.WriteLine(dbChanged ? $"[{time.Hour}:{time.Minute}] New games were just fetched!" : $"[{time.Hour}:{time.Minute}] No new games were fetched");
+
+        Thread.Sleep(refreshRate);
+}
---
+++ backend/RasbetServer/APICache/lib/API.cs
@@ -0,0 +1,97 @@
+﻿using System.Globalization;
+using MySqlConnector;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace APICache.lib;
+
+public class Api {
+    private const string ApiUrl = "http://ucras.di.uminho.pt/v1/games/";
+
+    const string ConnectionString =
+        "Host=localhost;Username=root;Password=root;Database=rasbet"; // FIXME: Should be in env file
+
+    private readonly MySqlConnection _dataSource;
+
+    public Api() {
+        _dataSource = new MySqlConnection(ConnectionString);
+        _dataSource.Open();
+    }
+
+
+    public async Task<string> FetchGames(HttpClient client) {
+        return await client.GetStringAsync(ApiUrl);
+    }
+
+    // TODO: Needs cleanup
+    public bool WriteToDatabase(string data) {
+        JArray json = JArray.Parse(data);
+        bool dbChanged = false;
+        
+        foreach (var game in json)
+        {
+            var jobject = game.ToObject<JObject>();
+
+            string id = jobject["id"].Value<string>();
+            string home = null; 
+            string away = null;
+            float priceHome = 0;
+            float priceAway = 0;
+            float priceDraw = 0;
+            
+            
+            away = jobject["awayTeam"].Value<string>();
+
+            var outcomes = jobject["bookmakers"].ToObject<JArray>()
+                .First["markets"]
+                .First["outcomes"]
+                .ToObject<JArray>();
+            
+            foreach (var odd in outcomes)
+            {
+                var team = odd["name"].Value<string>();
+                var price = odd["price"].Value<float>();
+
+                if (team == away)
+                    priceAway = price;
+                else if (team == "Draw")
+                    priceDraw = price;
+                else
+                {
+                    home = team;
+                    priceHome = price;
+                }
+            }
+
+            if (home is null || away is null)
+                throw new JsonException();
+
+            try
+            {
+                ExecuteInsert(id, home, priceHome, away, priceAway, priceDraw);
+                dbChanged = true;
+            }
+            catch (MySqlException e)
+            { }
+        }
+
+        return dbChanged;
+    }
+
+    private void ExecuteInsert(string id, string home, float homePrice, string away, float awayPrice, float drawPrice)
+    {
+        var insert = 
+            "INSERT INTO TwoTeamParticipants(id, Home, HomePrice, Away, AwayPrice, DrawPrice)" +
+                "VALUES (" +
+                $"'{id}'," +
+                $"'{home}'," +
+                $"{homePrice.ToString(CultureInfo.InvariantCulture)}," +
+                $"'{away}'," +
+                $"{awayPrice.ToString(CultureInfo.InvariantCulture)}," +
+                $"{drawPrice.ToString(CultureInfo.InvariantCulture)}" +
+            ")";
+        
+        var cmd = new MySqlCommand(insert, _dataSource);
+        cmd.ExecuteNonQuery();
+    }
+}
