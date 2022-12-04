e2e8393e5e40cc0bc3aa15732fa9ed95d1521cff
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2022-12-04 18:19:25 +0000

    Add useEvents Progress Loader. Add more information in the UI about the useEvents hook.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,45 +1,10 @@
 import type {NextPage} from 'next';
-import {EventCard, EventCardProps} from '@components/EventCard';
+import {EventCard} from '@components/EventCard';
 import {PageLayout} from '@components/PageLayout';
 import {useEvents} from '@hooks/useEvents';
+import {CircularProgress} from '@mui/material';
 
-/* FIXME Mock data hardcoded */
-const MOCK_EVENT: EventCardProps = {
-    eventId: '1',
-    eventName: 'Real Madrid vs Barcelona',
-    eventType: 'football',
-    commenceTime: new Date().toString(),
-    odds: [
-        {
-            name: 'Real Madrid',
-            price: 1.54,
-        },
-        {
-            name: 'Empate',
-            price: 2.57,
-        },
-        {
-            name: 'Barcelona',
-            price: 3.59,
-        },
-    ],
-};
-
-const MOCK_EVENTS: EventCardProps[] = [
-    MOCK_EVENT,
-    MOCK_EVENT,
-    MOCK_EVENT,
-    MOCK_EVENT,
-    MOCK_EVENT,
-    MOCK_EVENT,
-    MOCK_EVENT,
-    MOCK_EVENT,
-];
-
-const PRIMARY_SPORT_ID = '0'; // Football
 const PRIMARY_COMPETITION_ID = '0'; // Football
-
-const EVENT_ID = '08508dce-5873-4c44-8df0-f1df13187225';
 
 const Home: NextPage = () => {
     const {isSuccess, isLoading, isError, events, error} = useEvents(
@@ -48,11 +13,16 @@ const Home: NextPage = () => {
 
     return (
         <PageLayout>
-            <div className="flex flex-col justify-start gap-3 w-full">
+            <div className="flex flex-col justify-start items-center gap-3 w-full">
+                {isLoading && <CircularProgress />}
                 {isSuccess &&
                     events.map((event) => (
                         <EventCard key={event.Id} {...event} />
                     ))}
+                {isSuccess && events.length === 0 && (
+                    <span>Não há jogos por agora</span>
+                )}
+                {isError && <span>{error}</span>}
             </div>
         </PageLayout>
     );
