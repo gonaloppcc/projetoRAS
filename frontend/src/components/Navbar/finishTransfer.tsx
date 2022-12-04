9fee089ce88c863c1c42a9c179bc082da79b7da7
barbarafreixo20 <114660672+barbarafreixo20@users.noreply.github.com>
2022-12-04 22:59:09 +0000

    update

--- frontend/src/components/Navbar/finishTransfer.tsx
+++ frontend/src/components/Navbar/finishTransfer.tsx
@@ -1,9 +1,12 @@
+import {FormattedMessage, useIntl} from 'react-intl';
 export const FinishTransfer = (props) => {
+    const intl = useIntl();
+    const featureTransfer = intl.formatMessage({id: 'Navbar.finishTransfer'});
     return (
         // FIXME
         //<div className="bg-white w-fit text-xl text-center px-10 py-10  ">
         <div className="bg-white flex items-center font-semibold justify-center px-5 py-10 ">
-            Transferência efetuada com sucesso!
+            {featureTransfer}
         </div>
     );
 };
