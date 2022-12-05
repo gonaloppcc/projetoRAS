3e21c1da8fc4f8f7d31f3ac81af56a2ba1ab4dcf
Mapc1 <marcocosta579@gmail.com>
2022-12-05 00:35:31 +0000

    Readd events to Bets

--- backend/RasbetServer/RasbetServer/Models/Bets/OddBetIds.cs
+++ backend/RasbetServer/RasbetServer/Models/Bets/OddBetIds.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json.Linq;
 using RasbetServer.Models.Bets.Odds;
+using RasbetServer.Models.Events;
 
 namespace RasbetServer.Models.Bets;
 
@@ -11,12 +12,18 @@ public class OddBetIds
     public string OddId { get; set; }
     public virtual Odd Odd { get; set; }
     
+    [Required]
+    [ForeignKey("EventId")]
+    public string EventId { get; set; }
+    public virtual Event Event { get; set; }
+    
     [ForeignKey("BetId")] public string? MultiBetId { get; set; } = null;
 
     public static OddBetIds FromJson(JObject json)
     {
         string oddId = json[nameof(OddId)].Value<string>();
+        string eventId = json[nameof(EventId)].Value<string>();
 
-        return new OddBetIds { OddId = oddId };
+        return new OddBetIds { OddId = oddId, EventId = eventId };
     }
 }
