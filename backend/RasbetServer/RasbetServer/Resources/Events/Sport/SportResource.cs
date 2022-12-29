80844744e1922a5fb1bc833c6bc98d2a6fcf270d
Mapc1 <marcocosta579@gmail.com>
2022-12-29 23:51:07 +0000

    Add players and teams to SportResource

--- backend/RasbetServer/RasbetServer/Resources/Events/Sport/SportResource.cs
+++ backend/RasbetServer/RasbetServer/Resources/Events/Sport/SportResource.cs
@@ -1,7 +1,11 @@
+using RasbetServer.Models.Events.Participants.Participant;
+
 namespace RasbetServer.Resources.Events.Sport;
 
 public class SportResource
 {
     public string Name { get; set; }
     public virtual IEnumerable<string> Competitions { get; set; }
+    public virtual IEnumerable<string> Teams { get; set; }
+    public virtual IEnumerable<string> Players { get; set; }
 }
