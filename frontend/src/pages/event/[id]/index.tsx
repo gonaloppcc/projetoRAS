0ab2954ca50d6bdfcf06a2ec18f74cee4db6cda8
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2023-01-16 19:11:46 +0000

    Add Event Page header.

--- frontend/src/pages/event/[id]/index.tsx
+++ frontend/src/pages/event/[id]/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {NextPage} from 'next';
 import {PageLayout} from '@components/PageLayout';
 import {useEvent} from '@hooks/useEvent';
+import {formatDate} from '../../../utils/formatters';
+import {SportIcon} from '@components/SportIcon';
 
 interface PageProps {
     eventId: string;
@@ -12,14 +14,26 @@ const EventPage: NextPage<PageProps> = ({eventId}) => {
 
     return (
         <PageLayout>
-            <div className="flex flex-col justify-start items-center px-4 min-w-min w-full h-full drop-shadow-sm rounded bg-WHITE gap-5 p-4">
+            <div className="flex flex-col justify-start items-center px-4 min-w-min w-full drop-shadow-sm rounded gap-5">
                 {isLoading && <span>Loading...</span>}
+                {isError && <span>{error?.message}</span>}
                 {isSuccess && (
-                    <span>
-                        {event.participants.home.participant.participantName}
-                        {' - '}
-                        {event.participants.away.participant.participantName}
-                    </span>
+                    <div className="w-full flex flex-col justify-start items-center bg-white p-4 gap-3">
+                        <div className="w-full flex flex-row justify-start items-center gap-2">
+                            <SportIcon eventType={event.sportId} />
+                            <span className="text-xs ">
+                                {event.competition}
+                            </span>
+                        </div>
+                        <div className="w-full flex flex-col justify-start item-center gap-2">
+                            <span className="text-2xl font-bold text-center">
+                                {`${event.participants.home.participant.participantName} - ${event.participants.away.participant.participantName}`}
+                            </span>
+                            <span className="text-lg text-center">
+                                {formatDate(event.date)}
+                            </span>
+                        </div>
+                    </div>
                 )}
             </div>
         </PageLayout>
