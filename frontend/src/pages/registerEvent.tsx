51cc143f9654a9b80ef91f2028d204559e2d9499
Diogo Manuel Brito Pires <30441515+El-Banderas@users.noreply.github.com>
2023-01-17 02:28:52 +0000

    limpar coisas

--- frontend/src/pages/registerEvent.tsx
+++ frontend/src/pages/registerEvent.tsx
@@ -1,32 +1,7 @@
 import type {NextPage} from 'next';
 import {CircularProgress} from '@mui/material';
 import {RegisterEvent} from '@components/RegisterEvent';
-import {InfoSport} from '@domain/Event';
 import {useSports} from '@hooks/useSports';
-
-/* FIXME Mock data hardcoded */
-const MOCK_SPORTS: InfoSport[] = [
-    {
-        name: 'Football',
-        competitions: ['Premier League', 'La Liga', 'Serie A', 'Bundesliga'],
-        teams: ['Benfica', 'Porto', 'Real Madrid', 'Varzim', 'Rio Ave'],
-    },
-    {
-        name: 'Basketball',
-        competitions: ['NBA', 'Liga ACB', 'Liga Portuguesa'],
-        teams: [
-            'LA Lakers',
-            'Boston Celtics',
-            'Golden State Warriors',
-            'Milwaukee Bucks',
-        ],
-    },
-    {
-        name: 'Karate',
-        competitions: ['World Karate Federation', 'World Karate Federation2'],
-        teams: ['Benfica', 'Porto', 'Real Madrid', 'Varzim', 'Rio Ave'],
-    },
-];
 
 const Home: NextPage = () => {
     const {isSuccess, isLoading, isError, sports, error} = useSports();
