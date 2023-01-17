598ff8c0fd9bd5bdb7fe4597325f58fe5197c8d8
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2023-01-17 22:39:44 +0000

    Add close icon to remove notification.

--- frontend/src/components/Notification/index.tsx
+++ frontend/src/components/Notification/index.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import {Notification as NotificationType} from '@domain/User';
 import {formatDate} from '../../utils/formatters';
+import {XMarkIcon} from '@heroicons/react/20/solid';
 
-export type NotificationProps = NotificationType;
+export interface NotificationProps extends NotificationType {
+    deleteNotification: () => void;
+}
 
-export const Notification = ({message, date, severity}: NotificationProps) => {
+export const Notification = ({
+    message,
+    date,
+    severity,
+    deleteNotification,
+}: NotificationProps) => {
     return (
-        <div className="bg-WHITE flex flex-row justify-between items-center p-4 rounded-md border border-BLACK_SHADOW">
-            <div className="flex flex-col gap-2">
+        <div className="bg-WHITE flex flex-col justify-between items-center p-4 rounded-md border border-BLACK_SHADOW">
+            <div className="w-full flex flex-row gap-2 justify-between">
                 <div className="text-lg font-medium leading-none">
                     {message}
                 </div>
+                <XMarkIcon
+                    className="h-6 w-6 cursor-pointer"
+                    aria-hidden="true"
+                    onClick={deleteNotification}
+                />
+            </div>
+            <div className="w-full flex flex-row justify-between items-center">
                 <div className="text-xs text-gray-600 leading-none">
                     {formatDate(date)}
                 </div>
-            </div>
-            <div className="flex flex-row justify-center items-center">
-                <div className="text-sm text-gray-500">{severity}</div>
             </div>
         </div>
     );
