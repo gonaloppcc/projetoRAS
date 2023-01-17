fb6eede82a7c6d3f073d17c8a4bca89b275d9bcc
Gonçalo André Rodrigues Soares <62038187+gonaloppcc@users.noreply.github.com>
2023-01-17 01:38:45 +0000

    Fix register. Add login, redirect and success toast after register.

--- frontend/src/services/backend/user.ts
+++ frontend/src/services/backend/user.ts
@@ -1,4 +1,4 @@
-import {User, Specialist} from '@domain/User';
+import {Specialist, User} from '@domain/User';
 import {BASE_URL} from './constants';
 import axios from 'axios';
 
@@ -10,14 +10,21 @@ export const login = async (email: string, password: string): Promise<User> => {
     return response.data as User;
 };
 
-export const register = async (
+export const registerBetter = async (
+    username: string,
     email: string,
-    password: string
+    password: string,
+    nif: string,
+    cellphone: string,
+    cc: string
 ): Promise<User> => {
-    const response = await axios.post(`${BASE_URL}/users`, {
+    const response = await axios.post(`${BASE_URL}/users/betters`, {
+        username,
         email,
         password,
-        // TODO: ADD REST OF ARGUMENTS
+        nif,
+        cellphone,
+        cc,
     });
     return response.data as User;
 };
